322a83a84d12cb612ceca0398a294aebad3c7e4f
trinity-thiele <trin.thiele@gmail.com>
2025-04-01 17:19:58 -0400

    Got rid of ATM Views

--- CSC-429-Project-main/Basic Project Repository/userinterface/ViewFactory.java
+++ CSC-429-Project-main/Basic Project Repository/userinterface/ViewFactory.java
@@ -7,70 +7,21 @@ public class ViewFactory {
 
 	public static View createView(String viewName, IModel model)
 	{
-		if(viewName.equals("TellerView") == true)
-		{
-			return new TellerView(model);
-		}
-		else if(viewName.equals("TransactionChoiceView") == true)
-		{
-			return new TransactionChoiceView(model);
-		} else if (viewName.equals("AddScoutView") == true) {
-			return new AddScoutView(model);
-		} else if (viewName.equals("ModifyScoutView") == true){
-			return new ModifyScoutView(model);
-		} else if (viewName.equals("RemoveScoutView") == true){
-			return new RemoveScoutView(model);
-		}
-		else if(viewName.equals("AccountCollectionView") == true)
-		{
-			return new AccountCollectionView(model);
-		}
-		else if(viewName.equals("AccountView") == true)
-		{
-			return new AccountView(model);
-		}
-		else if(viewName.equals("AccountHolderIDEntryView") == true)
-		{
-			return new AccountHolderIDEntryView(model);
-		}
-		else if(viewName.equals("DepositTransactionView") == true)
-		{
-			return new DepositTransactionView(model);
-		}
-		else if(viewName.equals("DepositAmountView") == true)
-		{
-			return new DepositAmountView(model);
-		}
-		else if(viewName.equals("WithdrawTransactionView") == true)
-		{
-			return new WithdrawTransactionView(model);
-		}
-		else if(viewName.equals("TransferTransactionView") == true)
-		{
-			return new TransferTransactionView(model);
-		}
-		else if(viewName.equals("BalanceInquiryTransactionView") == true)
-		{
-			return new BalanceInquiryTransactionView(model);
-		}
-		else if(viewName.equals("BalanceInquiryReceipt") == true)
-		{
-			return new BalanceInquiryReceipt(model);
-		}
-		else if(viewName.equals("WithdrawReceipt") == true)
-		{
-			return new WithdrawReceipt(model);
-		}
-		else if(viewName.equals("DepositReceipt") == true)
-		{
-			return new DepositReceipt(model);
-		}
-		else if(viewName.equals("TransferReceipt") == true)
-		{
-			return new TransferReceipt(model);
-		}
-		else
-			return null;
+        switch (viewName) {
+            case "TransactionChoiceView":
+                return new TransactionChoiceView(model);
+
+            case "AddScoutView":
+                return new AddScoutView(model);
+
+            case "ModifyScoutView":
+                return new ModifyScoutView(model);
+
+            case "RemoveScoutView":
+                return new RemoveScoutView(model);
+            default:
+                return null;
+        }
 	}
 
 
